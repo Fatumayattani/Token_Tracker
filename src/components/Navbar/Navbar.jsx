5e4142a8b40e1c3cce261d84a1afcd2bd350ae85
Fatumayattani <fyattani@gmail.com>
2024-10-23 14:09:32 +0300

    update changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,19 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <img src={ttlogo} alt="" />
+        <ul>
+            <li>Home</li>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+        </ul>
+        <div className="nav-right">
+            <select>
+                <option value="usd">USD</option>
+                <option value="usd">USD</option>
+                <option value="usd">USD</option>
+            </select>
+        </div>
       
     </div>
   )
