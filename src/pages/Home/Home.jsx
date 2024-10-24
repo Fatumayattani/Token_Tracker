3343e91e800ff840fc47ac77760f8ef2912715eb
Fatumayattani <fyattani@gmail.com>
2024-10-24 11:38:01 +0300

    save changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,9 @@ const Home = () => {
       <div className="hero">
         <h1>Largest <br/> Crypto Marketplace</h1>
         <p>Welcome to the World's largest cryptocurrency marletplace. Sign up to explore more about cryptos.</p>
+        <form>
+          <input type="text" placeholder='Search crypto..' />
+        </form>
       </div>
       
     </div>
